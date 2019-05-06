ab039fd6de216e264c0fc53b4871a58306ec42ad
GONZALO RAFAEL NAVA VARGAS <gonzalo.nava@senado.gob.mx>
2019-05-06 14:35:54 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,45 @@
+
+const express = require('express')
+const app = express()
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+
+app.use( express.static( __dirname + '/public' ));
+
+// Express hbs engine
+hbs.registerPartials( __dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+// helpers
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+})
+
+ 
+app.get('/', (req, res) => {
+
+    res.render('home',{
+        nombre: 'Rafael'
+    });
+
+})
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+})
+
+// app.get('/data', (req, res) => {
+
+//       res.send('Hola Data');
+  
+//     })
+ 
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
